27d19fb85463bfa8f8053ae4467b71c7695653cb
Eugene <eugenevdm@gmail.com>
2022-05-14 11:10:55 +0200

    much more verbose update.sh

--- servers/libvirt/lib/update.sh
+++ servers/libvirt/lib/update.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# pwd
-# .../whmcs/modules/servers/libvirt/lib
+# Usage
+# pwd should be 'whmcs_installation/modules/servers/libvirt/lib'
+# sh update.sh
 
+echo "Libvirt Update Script"
+echo "====================="
+
+echo "Cloning repo"
 git clone https://github.com/fintech-systems/libvirt-whmcs
+echo "------------"
+
+echo "Copying addons to WHMCS installation location..."
 cp -r libvirt-whmcs/addons/libvirt/ ../../../addons/        
+echo "------------------------------------------------"
+
+echo "Copying servers to WHMCS installation location..."
 cp -r libvirt-whmcs/servers/libvirt ../../../servers/ 
+echo "-------------------------------------------------"
+
+echo "Removing clone"
 rm -rf libvirt-whmcs
+echo "--------------"
+
+echo "DONE"
